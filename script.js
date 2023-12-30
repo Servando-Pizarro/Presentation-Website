be35e758ac00d1702ab108519852900f77b3b9df
Servando <servandopizarr@web.de>
2023-12-30 14:55:35 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Add form validation logic
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    if (name && email && message) {
+        // Logic to handle form submission
+        console.log('Form submitted');
+    } else {
+        // Logic to handle validation error
+        console.log('Validation failed');
+    }
+});
